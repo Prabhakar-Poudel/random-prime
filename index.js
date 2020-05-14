1b632bd037f697400cb46c813fe132e14c8cc76e
Prabhakar Poudel <yuvrajzohan@gmail.com>
2020-05-14 22:17:41 +0200

    minor cleanup

--- index.js
+++ index.js
@@ -1,89 +1,64 @@
-'use-strict';
+'use-strict'
 
 const isPrime = number => {
-  if (typeof number !== 'number') {
-    throw new TypeError('Expected input of type Number');
-  }
+  if (typeof number !== 'number') throw new TypeError('Expected input of type Number')
 
-  if (number <= 3) {
-    return number > 1;
-  }
+  if (number <= 3) return number > 1
 
-  if (number % 2 === 0 || number % 3 === 0) {
-    return false;
-  }
+  if (number % 2 === 0 || number % 3 === 0) return false
 
-  const maxDivisor = Math.floor(Math.sqrt(number));
+  const maxDivisor = Math.floor(Math.sqrt(number))
   for (let i = 0; i <= maxDivisor; i += 6) {
     // eslint-disable-next-line no-mixed-operators
-    if (number % i === 0 || number % i + 2 === 0) {
-      return false;
-    }
+    if (number % i === 0 || number % i + 2 === 0) return false
   }
 
-  return true;
-};
+  return true
+}
 
 const randomPrime = (min, max) => {
   if (max === undefined) {
-    if (min === undefined) {
-      max = Number.MAX_SAFE_INTEGER;
-    } else {
-      max = min;
-      min = 0;
+    if (min === undefined) max = Number.MAX_SAFE_INTEGER
+    else {
+      max = min
+      min = 0
     }
   }
 
-  if (min === undefined) {
-    min = 0;
-  }
+  if (min === undefined) min = 0
 
   if (typeof min !== 'number' || typeof max !== 'number') {
-    throw new TypeError('Expected both arguments of type Number');
+    throw new TypeError('Expected both arguments of type Number')
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = Math.ceil(min)
+  max = Math.floor(max)
 
-  const range = max - min + 1;
-  const randomNum = Math.floor(Math.random() * range) + min;
+  const range = max - min + 1
+  const randomNum = Math.floor(Math.random() * range) + min
 
-  if (isPrime(randomNum)) {
-    return randomNum;
-  }
+  if (isPrime(randomNum)) return randomNum
 
-  let i = randomNum - 1;
-  let j = randomNum + 1;
+  let i = randomNum - 1
+  let j = randomNum + 1
   while (i >= min && j <= max) {
-    if (isPrime(i)) {
-      return i;
-    }
-
-    if (isPrime(j)) {
-      return j;
-    }
-
-    i -= 1;
-    j += 1;
+    if (isPrime(i)) return i
+    if (isPrime(j)) return j
+    i -= 1
+    j += 1
   }
 
   while (i >= min) {
-    if (isPrime(i)) {
-      return i;
-    }
-
-    i -= 1;
+    if (isPrime(i)) return i
+    i -= 1
   }
 
   while (j <= max) {
-    if (isPrime(j)) {
-      return j;
-    }
-
-    j += 1;
+    if (isPrime(j)) return j
+    j += 1
   }
 
-  return null;
-};
+  return null
+}
 
-module.exports = {isPrime, randomPrime};
+module.exports = {isPrime, randomPrime}
--- test.js
+++ test.js
@@ -1,62 +1,62 @@
-const random = require('.');
+const random = require('.')
 
-const {isPrime, randomPrime} = random;
+const {isPrime, randomPrime} = random
 
 describe('isPrime()', () => {
   it('returns true for prime numbers', () => {
-    expect(isPrime(2)).toBeTruthy();
-    expect(isPrime(3)).toBeTruthy();
-    expect(isPrime(101)).toBeTruthy();
-    expect(isPrime(15487403)).toBeTruthy();
-    expect(isPrime(32416190071)).toBeTruthy();
-  });
+    expect(isPrime(2)).toBeTruthy()
+    expect(isPrime(3)).toBeTruthy()
+    expect(isPrime(101)).toBeTruthy()
+    expect(isPrime(15487403)).toBeTruthy()
+    expect(isPrime(32416190071)).toBeTruthy()
+  })
   it('returns false for non-prime numbers', () => {
-    expect(isPrime(1)).toBeFalsy();
-    expect(isPrime(4)).toBeFalsy();
-    expect(isPrime(-7)).toBeFalsy();
-    expect(isPrime(15487402)).toBeFalsy();
-    expect(isPrime(32416190079)).toBeFalsy();
-  });
+    expect(isPrime(1)).toBeFalsy()
+    expect(isPrime(4)).toBeFalsy()
+    expect(isPrime(-7)).toBeFalsy()
+    expect(isPrime(15487402)).toBeFalsy()
+    expect(isPrime(32416190079)).toBeFalsy()
+  })
   it('throws error for invalid inputs', () => {
-    expect(() => isPrime()).toThrow(TypeError);
-    expect(() => isPrime('foo')).toThrow(TypeError);
-  });
-});
+    expect(() => isPrime()).toThrow(TypeError)
+    expect(() => isPrime('foo')).toThrow(TypeError)
+  })
+})
 
 describe('randomPrime()', () => {
   it('works for no input', () => {
-    expect(randomPrime()).toEqual(expect.anything());
-    expect(randomPrime()).toBeGreaterThan(1);
-    expect(randomPrime()).toBeLessThan(Number.MAX_SAFE_INTEGER);
-  });
+    expect(randomPrime()).toEqual(expect.anything())
+    expect(randomPrime()).toBeGreaterThan(1)
+    expect(randomPrime()).toBeLessThan(Number.MAX_SAFE_INTEGER)
+  })
   it('works for single (max) input', () => {
-    expect(randomPrime(2)).toBe(2);
-    expect(randomPrime(10)).toBeLessThan(10);
-    expect(randomPrime(100)).toBeLessThan(100);
-    expect(randomPrime(10)).toBeGreaterThan(1);
-  });
+    expect(randomPrime(2)).toBe(2)
+    expect(randomPrime(10)).toBeLessThan(10)
+    expect(randomPrime(100)).toBeLessThan(100)
+    expect(randomPrime(10)).toBeGreaterThan(1)
+  })
   it('works for min, max inputs', () => {
-    expect(randomPrime(3, 4)).toBe(3);
-    expect(randomPrime(4, 5)).toBe(5);
-    expect(randomPrime(14, 18)).toBe(17);
-    expect(randomPrime(10, 100)).toBeGreaterThan(10);
-    expect(randomPrime(10, 100)).toBeLessThan(100);
-    expect(randomPrime(7, 7)).toBe(7);
-    expect(randomPrime(-10, 10)).not.toBeNull();
-    expect(randomPrime(-10, 2)).toBe(2);
-  });
+    expect(randomPrime(3, 4)).toBe(3)
+    expect(randomPrime(4, 5)).toBe(5)
+    expect(randomPrime(14, 18)).toBe(17)
+    expect(randomPrime(10, 100)).toBeGreaterThan(10)
+    expect(randomPrime(10, 100)).toBeLessThan(100)
+    expect(randomPrime(7, 7)).toBe(7)
+    expect(randomPrime(-10, 10)).not.toBeNull()
+    expect(randomPrime(-10, 2)).toBe(2)
+  })
   it('returns null if no prime number exist', () => {
-    expect(randomPrime(1)).toBeNull();
-    expect(randomPrime(-10)).toBeNull();
-    expect(randomPrime(-10, 1)).toBeNull();
-    expect(randomPrime(0)).toBeNull();
-    expect(randomPrime(10, 10)).toBeNull();
-    expect(randomPrime(14, 16)).toBeNull();
-  });
+    expect(randomPrime(1)).toBeNull()
+    expect(randomPrime(-10)).toBeNull()
+    expect(randomPrime(-10, 1)).toBeNull()
+    expect(randomPrime(0)).toBeNull()
+    expect(randomPrime(10, 10)).toBeNull()
+    expect(randomPrime(14, 16)).toBeNull()
+  })
   it('throws error for non numeric inputs', () => {
-    expect(() => randomPrime('foo')).toThrow(TypeError);
-    expect(() => randomPrime(23, 'foo')).toThrow(TypeError);
-    expect(() => randomPrime('foo', 'bar')).toThrow(TypeError);
-  });
-});
+    expect(() => randomPrime('foo')).toThrow(TypeError)
+    expect(() => randomPrime(23, 'foo')).toThrow(TypeError)
+    expect(() => randomPrime('foo', 'bar')).toThrow(TypeError)
+  })
+})
 
